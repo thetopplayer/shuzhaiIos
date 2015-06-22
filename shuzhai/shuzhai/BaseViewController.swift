2b6a2b4898ed85b888bef32dd65aca2f47d2f62a
chhe <chhe@ebay.com>
2015-06-22 18:29:43 -0400

    commit

--- shuzhai/shuzhai/BaseViewController.swift
+++ shuzhai/shuzhai/BaseViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
+
 class BaseViewController: UIViewController {
 
     var tabButtonViewController:TabButtonViewController!
+    @IBOutlet var barButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.hidden = true
         tabButtonViewController = TabButtonViewController(nibName:"TabButtonViewController", bundle:nil)
         self.view.addSubview(tabButtonViewController.view)
+        
+        let navBar = self.navigationController!.navigationBar
+        navBar.barTintColor = UIColor(red: 65.0 / 255.0, green: 62.0 / 255.0, blue: 79.0 / 255.0, alpha: 1)
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
 
         // Do any additional setup after loading the view.
     }
@@ -24,6 +30,16 @@ class BaseViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Your Menu View Controller vew must know the following data for the proper animatio
+        let destinationVC = segue.destinationViewController as! GuillotineMenuViewController
+        destinationVC.hostNavigationBarHeight = self.navigationController!.navigationBar.frame.size.height
+        destinationVC.hostTitleText = self.navigationItem.title
+        destinationVC.view.backgroundColor = self.navigationController!.navigationBar.barTintColor
+        destinationVC.setMenuButtonWithImage(barButton.imageView!.image!)
     }
     
 
